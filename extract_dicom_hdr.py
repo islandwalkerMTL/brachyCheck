2e479a9edfc5b4f5beafe45b16c628fd31460ac6
Ellis <ooellis@gmail.com>
2018-02-01 14:40:29 -0500

    Update extract_dicom_hdr.py

--- extract_dicom_hdr.py
+++ extract_dicom_hdr.py
@@ -1,15 +1,33 @@
-import dicom, sys, os.path, numpy
+
 ####################################################################
 #
-#This script will be used to extract data from the dicom file
-#that the physicist uploads. It will later be compared to the
-#qatrack entry pre-treatment.
+# This script will be used to extract data from the dicom file
+# that the physicist uploads. It will later be compared to the
+# qatrack entry pre-treatment.
 #
-# Copyright: Ellis Mitrou (ellis.mitrou.chum@ssss.gouv.qc.ca)
-# Nov 2016
 #
+# Copyright (c) [2018] [Ellis Mitrou]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions: 
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 ####################################################################
 
+import dicom, sys, os.path, numpy
 
 HDR_results = dict() #Define an empty dictionary to put all the results in
 
